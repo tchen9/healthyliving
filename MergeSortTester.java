1f0c9dabf6c2bf2035819b2ecdebac520bc185ac
Bryan <bchan4@stuy.edu>
2017-02-14 21:20:34 -0500

    Added runtimes

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -8,12 +8,16 @@
   <INSERT YOUR EXECUTION TIME CATEGORIZATION OF MERGESORT HERE>
 
   Mean execution times for dataset of size n:
-  Batch size: <# of times each dataset size was run>
-  n=1       time: 
-  n=10      time: 
-  n=100     time: 
+  Batch size: 1000 <# of times each dataset size was run>
+  n=1       time: 549ns
+  n=10      time: 2,619ns
+  n=100     time: 18,454ns
+  
   ...
-  n=<huge>  time: 
+  n=50      time: 8,336ns;
+  n=500     time: 84,077ns
+  n=1000    time: 156,362ns
+  n= 100,000<huge>  time: 802,704ns
 
   ANALYSIS:
   <INSERT YOUR RESULTS ANALYSIS HERE>
@@ -30,7 +34,29 @@ public class MergeSortTester
      ******************************/
     public static void main( String[] args ) 
     {
+	int n = 500; //number of elements
+	int batchsize = 1000; //number of times method is ran
+	int sumofTime = 0;
 
+	for (int i = 0; i < batchsize; i++){
+	    
+	    int[] arr = new int[n];
+	    for(int j = 0; j < n; j++){
+		arr[j] = (int)(Math.random() * n);
+	    }//populates array arr with n elements
+       
+	    long start = System.nanoTime();
+	    MergeSort.sort(arr);
+	    long elasped = System.nanoTime() - start;
+	
+	    System.out.println("Elaspsed time: " + elasped);	    
+	    sumofTime += elasped;
+
+	}
+	
+	long average = sumofTime / batchsize;
+	System.out.println("Average time: " + average);
+       			  
     }//end main
 
 }//end class
